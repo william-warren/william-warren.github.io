585e637d794fc8904d852685a3c24880972b3edb
Student <william.warren1019@yahoo.com>
2019-11-05 15:59:06 -0600

    Took JS out of html file

--- index.js
+++ index.js
@@ -19,4 +19,16 @@ function initializeAccordion() {
         }
     }
 }
+
+function CopyEmail() {
+    var copyText = document.querySelector(".button-email").title;
+    document.addEventListener('copy', function (event) {
+        event.clipboardData.setData('text/plain', copyText);
+        event.preventDefault();
+        document.removeEventListener('copy', handler, true);
+    }, true);
+    document.execCommand('copy');
+    alert("Copied: " + copyText);
+}
+
 initializeAccordion();
