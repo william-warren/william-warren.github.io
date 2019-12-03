7b0bf59e86c15e3bdcc920b1079857c7e7d238aa
Student <william.warren1019@yahoo.com>
2019-12-03 11:02:59 -0600

    Removed Accordion in favor of open grid display.

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-function initializeAccordion() {
-    const accordions = document.querySelectorAll(".accordion");
-    for (const accordion of accordions) {
-        const rows = accordion.querySelectorAll(".accordion-category-header");
-        for (const row of rows) {
-            row.addEventListener("click", function openThisItem() {
-                const item = event.target.closest(".accordion-category");
-                const accordion = event.target.closest(".accordion");
-                const items = accordion.querySelectorAll(".accordion-category");
-                if (item.classList.contains("accordion-category--open")) {
-                    item.classList.remove("accordion-category--open");
-                } else {
-                    for (const sibling of items) {
-                        sibling.classList.remove("accordion-category--open");
-                    }
-                    item.classList.add("accordion-category--open");
-                }
-            })
-        }
-    }
-}
-
 function CopyEmail() {
     var copyText = document.querySelector(".button-email").title;
     document.addEventListener('copy', function (event) {
@@ -30,5 +8,3 @@ function CopyEmail() {
     document.execCommand('copy');
     alert("Copied: " + copyText);
 }
-
-initializeAccordion();
